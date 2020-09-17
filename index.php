e219e095ab37959000f67ddbf1070b08474bbba3
Valerio <valerio.modesti@gmail.com>
2020-09-17 16:30:36 +0200

    html + css

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+     <head>
+         <meta charset="utf-8">
+         <title>Google-faq</title>
+         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600;700&display=swap" rel="stylesheet">
+         <link rel="stylesheet" href="css/style.css">
+     </head>
+     <body>
+         <header>
+             <div class="header-top">
+                 <div class="logo">
+                     <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_74x24dp.png" alt="google">
+                 </div>
+                 <div class="privacy">
+                     <a href="#">Privacy e termini</a>
+                 </div>
+             </div>
+             <div class="header-bottom">
+                <div class="navbar">
+                    <ul>
+                        <li>Introduzione</li>
+                        <li>Norme sulla privacy</li>
+                        <li>Termini di servizio</li>
+                        <li>Tecnologie</li>
+                        <li>Domande frequenti</li>
+                    </ul>
+                    <div class="user">
+                        <div class="user-img">
+                            <img src="https://icon-library.com/images/google-user-icon/google-user-icon-7.jpg" alt="user-img">
+                        </div>
+                        <div class="user-mail">francesco.totti@gmail.com</div>
+                    </div>
+                </div>
+             </div>
+         </header>
+         <main>
+             <div class="container">
+
+             </div>
+         </main>
+     </body>
+ </html>
